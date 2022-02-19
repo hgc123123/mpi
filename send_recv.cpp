7ff1bb4bcd2e9ae6485ab771443724b29a167890
胡广超 <hugc@sjtu.edu.cn>
2022-02-19 09:35:56 +0800

    add info

---
+++ send_recv.cpp
@@ -0,0 +1,21 @@
+#include<mpi.h>
+#include<iostream>
+int main(int argc,char **argv){
+    int rank,buf;
+    MPI_Status status;
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    if(rank==0) {
+        int buf = 12345;
+        MPI_Send(&buf, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    }else if(rank==1){
+        MPI_Recv(&buf,1,MPI_INT,0,0,MPI_COMM_WORLD,&status);
+        cout<<buf<<endl;
+    }
+    MPI_Finalize();
+    return 0;
+}
+
+[hpc@cas006 C_test]$ mpic++ send_recv.c -o send_recv
+[hpc@cas006 C_test]$ mpirun --oversubscribe  -np 2 ./send_recv
+12345
