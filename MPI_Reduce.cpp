0c7b1f446809fcb20d5b11b5cbc7bd9006fab11b
胡广超 <hugc@sjtu.edu.cn>
2022-02-21 14:39:49 +0800

    add info

---
+++ MPI_Reduce.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<mpi.h>
+#include<assert.h>
+#include<time.h>
+using namespace std;
+
+float *create_rand_nums(int num_elements){
+    float *rand_nums=new float[num_elements];
+    int i;
+    for (i = 0; i < num_elements; ++i) {
+        rand_nums[i]=(rand()/(float)RAND_MAX);
+    }
+    return rand_nums;
+}
+int main(int argc,char **argv){
+    int num_elements=atoi(argv[1]);
+    int nprocs,myrank;
+    int i;
+    MPI_Init(NULL,NULL);
+    MPI_Status status;
+    MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD,&myrank);
+    srand(time(NULL)*myrank);
+    float *rand_nums=NULL;
+    rand_nums=create_rand_nums(num_elements);
+
+    float localSum=0;
+    for (i = 0; i < num_elements; ++i) {
+        cout<<rand_nums[i]<<" ";
+        localSum+=rand_nums[i];
+    }
+    cout<<endl;
+    float globalSum=0;
+    MPI_Reduce(&localSum,&globalSum,1,MPI_FLOAT,MPI_SUM,0,MPI_COMM_WORLD);
+    if(myrank==0){
+        cout<<"global sum is: "<<globalSum<<endl;
+    }
+    cout<<endl;
+    delete []rand_nums;
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+}
